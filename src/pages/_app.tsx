6409502e4ab31839e5fa8da053fd56bb795a2417
songchez <tama4840@gmail.com>
2023-02-11 11:39:30 +0900

    new

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
-import Layout from "@/components/Layout";
+import Base from "@/components/Base";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
+  function Kakao() {
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY);
+    console.log(window.Kakao.isInitialized());
+  }
   return (
-    <Layout>
+    <Base>
       <Component {...pageProps} />
-    </Layout>
+      <Script
+        src="https://developers.kakao.com/sdk/js/kakao.js"
+        onLoad={Kakao}
+      ></Script>
+    </Base>
   );
 }
