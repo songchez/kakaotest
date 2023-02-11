1e9721c0f22e9ef4f4043c47ded9e3cce386dd2b
songchez <tama4840@gmail.com>
2023-02-11 14:16:54 +0900

    new

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -4,8 +4,8 @@ export default function Base({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="카카오테스트테스트" />
+        <title>카카오테스트테스트</title>
+        <meta name="description" content="카카오테스트테스트입니다요" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,30 @@ export default function Home() {
       channelPublicId: "_atExexj",
     });
   }
-  function addbtn() {
-    window.Kakao.Channel.createChatButton({
-      container: "#kakao-talk-channel-chat-button",
-      channelPublicId: "_atExexj",
-      title: "consult",
-      size: "small",
-      color: "yellow",
-      shape: "pc",
-      supportMultipleDensities: true,
-    });
-  }
 
   return (
     <>
       <main>
         <div>
-          아무거나
           <a id="add-channel-button" onClick={addChannel}>
+            <Image
+              src="/friendadd_large_yellow_rect.png"
+              height={40}
+              width={100}
+              alt="카카오톡 채널 추가하기 버튼"
+            />
+          </a>
+        </div>
+        <div>
+          <a
+            id="kakao-talk-channel-add-button"
+            href="http://pf.kakao.com/_atExexj/chat"
+          >
             <Image
               src="/consult_small_yellow_pc.png"
               height={50}
               width={100}
-              alt="카카오톡 채널 추가하기 버튼"
+              alt="톡상담"
             />
           </a>
         </div>
